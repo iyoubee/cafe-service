51eafa1c322ded153916a1c120057f5d2c14de96
Nicholas Sidharta <toxicancm@gmail.com>
2023-04-11 00:20:09 +0700

    Nicholas/add CORS

--- src/main/java/id/ac/ui/cs/advprog/cafeservice/controller/MenuItemController.java
+++ src/main/java/id/ac/ui/cs/advprog/cafeservice/controller/MenuItemController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/cafe/menu")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class MenuItemController {
     private final MenuItemService menuItemService;
 
