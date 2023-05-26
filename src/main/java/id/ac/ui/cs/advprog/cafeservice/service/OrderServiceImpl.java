aac85f9f23043da311ed5a7e87f3da36f5884bbf
iyoubee <al.ayubi2020@gmail.com>
2023-05-26 11:37:27 +0700

    Re-try : hide credentials info with env variables #2

--- src/main/java/id/ac/ui/cs/advprog/cafeservice/service/OrderServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/cafeservice/service/OrderServiceImpl.java
@@ -18,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
